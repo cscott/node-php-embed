c9a374ab1737dd4fe3c4f2e8e334f98105065e8f
C. Scott Ananian <cscott@cscott.net>
2015-10-20 12:02:15 -0400

    Listen for SIGABRT in addition to SIGSEGV.
    Our OSX builds seem to crash with SIGABRT.

--- test-setup.js
+++ test-setup.js
@@ -1,2 +1,5 @@
 var SegfaultHandler = require('segfault-handler');
+// Listen for SIGSEGV.
 SegfaultHandler.registerHandler("segfault.log");
+// Listen for SIGABRT, too.
+SegfaultHandler.registerHandler(SegfaultHandler.SIGABRT);
