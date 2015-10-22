92a1db77e489dbe120ae7445ab64510ac31223db
C. Scott Ananian <cscott@cscott.net>
2015-10-22 18:36:33 -0400

    Add ability to patch the distributed PHP.
    This can be useful for debugging crashes deep inside PHP.

--- deps/extract.py
+++ deps/extract.py
@@ -1,11 +1,16 @@
 import sys
 import tarfile
 import os
+import subprocess
 
 tarball = os.path.abspath(sys.argv[1])
 dirname = os.path.abspath(sys.argv[2])
 print "Extracting", tarball, "to", dirname
 tfile = tarfile.open(tarball,'r:gz');
 tfile.extractall(dirname)
+# Optionally, apply a patch.  This can be useful when debugging
+# crashes deep inside php.
+if os.path.isfile(tarball + '.patch'):
+    subprocess.check_call(['patch', '-d', dirname, '-i', tarball + '.patch', '-p', '0']);
 os.utime(os.path.join(dirname, sys.argv[3]), None) # touch
 sys.exit(0)
