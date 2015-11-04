136a7f35944c1937109c24a752553f246d49d673
C. Scott Ananian <cscott@cscott.net>
2015-11-04 17:02:03 -0500

    Release 0.5.3.

--- src/node_php_embed.h
+++ src/node_php_embed.h
@@ -7,7 +7,7 @@
 #define NODE_PHP_EMBED_NODE_PHP_EMBED_H_
 
 // This should match the version declared in package.json.
-#define NODE_PHP_EMBED_VERSION "0.5.2-git"
+#define NODE_PHP_EMBED_VERSION "0.5.3"
 
 extern "C" {
 #include "TSRM/TSRM.h"
